8fba847006cc7a3789f7781b460a8bd5d8f253bf
dsamuel32 <diego.samuel.alves@gmail.com>
2018-10-29 15:30:02 -0300

    Ajustando carga de modulos

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
 import Servidor from './config/servidor';
-import informacoes from './routes/informacoes';
-import comandos from './routes/comandos'
 
-const routes = [informacoes, comandos];
 new Servidor().start();
